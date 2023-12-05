852eb2daf1d75b1883714bab3efbd15e0e3afb8d
TERRENCE  MASHEGO <TERRENCE  MASHEGO@USERDELLFLOOR2>
2023-12-05 15:53:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Building_A_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example: converting a string to an integer
+            int num = Convert.ToInt32("45");
+            Console.WriteLine("Result: " + (num + 6));
+
+            // Taking user input for two integers
+            Console.WriteLine("Enter the first integer:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second integer:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Taking user input for two doubles
+            Console.WriteLine("Enter the first double:");
+            double num3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the second double:");
+            double num4 = Convert.ToDouble(Console.ReadLine());
+
+            // Displaying the sum of two doubles
+            Console.WriteLine("Sum of the doubles: " + (num3 + num4));
+
+            Console.Read();
+        }
+    }
+}
